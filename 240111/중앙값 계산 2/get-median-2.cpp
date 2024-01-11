b7e9cd0ac22bf66258da237530aee9496a629874
phjh <97834885+phjh@users.noreply.github.com>
2024-01-11 10:48:12 +0900

    Title: 중앙값 계산 2, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240111/중앙값 계산 2/get-median-2.cpp
+++ 240111/중앙값 계산 2/get-median-2.cpp
@@ -1,17 +1,19 @@
 #include <iostream>
+#include<algorithm>
 using namespace std;
 
 int main() {
     int arr[101];
     int n;
-    cin>>n;
-    for(int i=0;i<n;i++){
+    cin >> n;
+    for (int i = 0; i < n; i++) {
         int temp;
-        cin>>temp;
+        cin >> temp;
         arr[i] = temp;
-        if(i%2==0){
-            cout<<arr[i/2]<<' ';
-        }
+        sort(arr, arr + i + 1);
+            if (i % 2 == 0) {
+                cout << arr[i / 2] << ' ';
+            }
     }
 
 
