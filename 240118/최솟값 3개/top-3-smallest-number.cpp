fcf43c681e8fb11a27a71fca4ce36bbe65fb56cf
phjh <97834885+phjh@users.noreply.github.com>
2024-01-18 11:53:32 +0900

    Title: 최솟값 3개, Time: 1073ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

---
+++ 240118/최솟값 3개/top-3-smallest-number.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+int main() {
+    priority_queue<int> queue;
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        int temp;
+        long long mult=1;
+        cin >> temp;
+        queue.push(temp);
+        priority_queue<int> tempqueue = queue;
+        if (queue.size() < 3){
+            cout << -1 << endl;
+            continue;
+        }
+        while (tempqueue.size() != 3) {
+            tempqueue.pop();
+        }
+        for (int i = 0; i < 3; i++) {
+            mult *= tempqueue.top();
+            tempqueue.pop();
+        }
+        cout << mult << endl;
+
+    }
+
+    return 0;
+}
