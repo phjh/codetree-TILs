baf3159d83bbd9ef0461d6f85b3ebf4a8864fa65
phjh <97834885+phjh@users.noreply.github.com>
2024-01-18 09:06:50 +0900

    Title: 숫자 빠르게 찾기, Time: 189ms, Memory: 0MB, Status: Passed - CodeTree

--- 240118/숫자 빠르게 찾기/find-number-fast.cpp
+++ 240118/숫자 빠르게 찾기/find-number-fast.cpp
@@ -1,31 +1,45 @@
 #include <iostream>
-#include <vector>
+
+#define MAX_N 100000
+
 using namespace std;
-int main() {
-    vector<int> v;
-    int n,m;
-    cin>>n>>m;
-    for(int i=0;i<n;i++){
-        int temp;
-        cin>>temp;
-        v.push_back(temp);
+
+int n, m;
+int arr[MAX_N];
+
+int Find(int target) {
+    int left = 0, right = n - 1;
+    while (left <= right) {
+        int mid = (left + right) / 2;
+        if(arr[mid] == target)
+            return mid;
+        
+        if(arr[mid] > target)
+            right = mid - 1;
+        else
+            left = mid + 1;
     }
-    for(int i=0;i<m;i++){
-        int temp,j;
-        bool found=false;
-        cin>>temp;
-        for(j=0;j<n;j++){
-            if(v[j] == temp){
-                found = true;
-                break;
-            }
-        }
-        if(found){
-            cout<< j+1 << endl;
-        }
-        else{
-            cout << -1<<endl;
-        }
+
+    return -1;
+}
+
+int main() {
+    // 입력
+    cin >> n >> m;
+    for(int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    for(int i = 0; i < m; i++) {
+        int x;
+        cin >> x;
+
+        // 이진탐색을 진행합니다.
+        int index = Find(x);
+
+        if(index >= 0)
+            cout << index + 1 << endl;
+        else
+            cout << -1 << endl;
     }
     return 0;
 }
